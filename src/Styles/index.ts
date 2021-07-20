0ef201cdd4f4d1aa77fccbcd5849befb23330f75
RamakrishnanVBI <45590205+RamakrishnanVBI@users.noreply.github.com>
2021-07-20 15:55:52 +0530

    CSS Update

--- src/Styles/index.ts
+++ src/Styles/index.ts
@@ -1,3 +1,8 @@
 import '../css/ToolbarButton.css';
 import '../css/DraftEditor.css';
 import '../css/Icon.css';
+
+//To Run Local 
+// import './ToolbarButton.less';
+// import './DraftEditor.less';
+// import './Icon.less';
