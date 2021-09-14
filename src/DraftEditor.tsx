d736b95c25d4747c4937862ff890e38de4168219
manikandanvengatesan <manikandan.vengatesan@gmail.com>
2021-09-14 12:25:18 +0530

    feat(Alignment):Text alignment option added

--- src/DraftEditor.tsx
+++ src/DraftEditor.tsx
@@ -28,6 +28,8 @@ interface IDraftEditorProps {
         value: string;
         mentionList: string[];
         rawValue?: string;
+        backgroundColor?: string;
+        justifyContent?: string;
     }) => void;
     onCurrentFormatChange?: (formats: IDraftElementFormats) => void;
     toolbarComponent?: ReactElement;
@@ -79,7 +81,11 @@ const PopOverContainer = (props) => {
               overflowY: 'auto',
           }
         : null;
-    return <div className="mention-list" style={style}>{props.children}</div>;
+    return (
+        <div className="mention-list" style={style}>
+            {props.children}
+        </div>
+    );
 };
 
 const mentionAnchorStyle: React.CSSProperties = {
@@ -211,11 +217,13 @@ class DraftEditor extends Component<IDraftEditorProps, any> {
                 formatKeys.fontSize,
                 formatKeys.background,
                 formatKeys.lineHeight,
+                formatKeys.justifyContent,
             ].includes(formatType)
         ) {
             nextEditorState = formatText(nextEditorState, formatType, `${formatType}__${value}`);
         } else nextEditorState = RichUtils.toggleInlineStyle(nextEditorState, formatType.toUpperCase());
-        this.updateData(nextEditorState);
+        const format = getFormat(nextEditorState);
+        this.updateData(nextEditorState, format);
     };
 
     onEditorStateChange = (editorStateUpdated: EditorState) => {
@@ -223,9 +231,9 @@ class DraftEditor extends Component<IDraftEditorProps, any> {
         this.updateData(editorStateUpdated);
     };
 
-    updateData = (editorStateUpdated: EditorState) => {
+    updateData = (editorStateUpdated: EditorState, customFormat?: any) => {
         const { onContentTextChange, onContentChange } = this.props;
-        const { peopleSearchOpen } = this.state;
+        const { peopleSearchOpen, format } = this.state;
         this.setState({
             editorState: editorStateUpdated,
         });
@@ -251,6 +259,8 @@ class DraftEditor extends Component<IDraftEditorProps, any> {
             value: htmlText,
             mentionList,
             rawValue: getContentFromEditorState(editorStateUpdated),
+            backgroundColor: customFormat?.backgroundColor ?? format?.backgroundColor,
+            justifyContent: customFormat?.justifyContent ?? format?.justifyContent,
         });
         onContentChange?.(htmlText);
     };
