bf8a0e090699409ef711f9f304825fffefae30dd
Karthick B <karthickbalagurucs@gmail.com>
2021-12-23 14:02:55 +0530

    mention component will be rendered in body

--- src/Components/PopOverContainer.tsx
+++ src/Components/PopOverContainer.tsx
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 
-export default class PopOverContainer extends Component<any> {
-    render() {
-        const boundingRect = this.props.store.getReferenceElement()?.getBoundingClientRect();
+const PopOverContainer = (props: any) => {
+        const boundingRect = props.store.getReferenceElement()?.getBoundingClientRect();
         const mentionWidth = 223;
         const clientWidth  = window.innerWidth || document.documentElement.clientWidth || 
         document.body.clientWidth;
         if (!boundingRect) {
             return null;
         }
+        let container =  document.body.getElementsByClassName('mention-list-container')[0];
+        if(!container) {
+           container = document.createElement('div');
+           container.className = 'mention-list-container'
+           document.body.appendChild(container);
+        }
+        useEffect(()=> {
+            return () => {
+                document.body.removeChild(container)
+            }
+        },[])
         const style: React.CSSProperties = boundingRect
             ? {
                   position: boundingRect ? 'fixed' : null,
-                  left: clientWidth - boundingRect?.left < mentionWidth ?  boundingRect?.left - mentionWidth :  boundingRect?.left + 20,
-                  top: boundingRect?.top + 15,
+                  left: clientWidth - boundingRect.left < mentionWidth ?  boundingRect.left - mentionWidth :  boundingRect.left + 20,
+                  top: boundingRect.top + 15,
                   backgroundColor: '#fff',
                   zIndex: 1000,
                   boxShadow: '0px 4px 4px rgb(0 0 0 / 25%)',
@@ -21,10 +32,18 @@ export default class PopOverContainer extends Component<any> {
                   overflowY: 'auto',
               }
             : null;
-        return (
-            <div className="mention-list" style={style}>
-                {this.props.children}
-            </div>
-        );
-    }
+
+        return createPortal((
+                <div className="mention-list" style={style}>
+                    {props.children}
+                </div>
+            ), container);  
+        // return (
+        //     <div className="mention-list" style={style}>
+        //         {props.children}
+        //     </div>
+        // );
+    
 }
+
+export default PopOverContainer;
