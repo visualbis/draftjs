96317204b725705faf87d95666c4f5c9fa15ad41
Raghu3130 <raghum@lumel.com>
2022-09-12 18:02:54 +0530

    optional data object condition added

--- src/DraftEditor.tsx
+++ src/DraftEditor.tsx
@@ -576,7 +576,7 @@ class DraftEditor extends Component<IDraftEditorProps, IDraftEditorState> {
 
         setTimeout(() => {
             // after adding selected text, reset focus ref
-            this.editorRef.current.focus();
+            this.editorRef.current?.focus();
         }, 200);
     };
 
@@ -628,7 +628,7 @@ class DraftEditor extends Component<IDraftEditorProps, IDraftEditorState> {
 
         setTimeout(() => {
             // after adding selected text, reset focus ref
-            this.editorRef.current.focus();
+            this.editorRef.current?.focus();
             onFocus && onFocus();
         }, 200);
         return newState;
@@ -662,7 +662,7 @@ class DraftEditor extends Component<IDraftEditorProps, IDraftEditorState> {
         this.updateData(EditorState.push(editorState, stateWithText, 'insert-fragment'));
         setTimeout(() => {
             // after adding selected text, reset focus ref
-            this.editorRef.current.focus();
+            this.editorRef.current?.focus();
             // onFocus && onFocus();
         }, 200);
     };
@@ -673,7 +673,7 @@ class DraftEditor extends Component<IDraftEditorProps, IDraftEditorState> {
 
     getEditorContainerRect = () => {
         if (this.editorRef.current) {
-            return this.editorRef.current.getEditorRef().editor.getBoundingClientRect();
+            return this.editorRef.current?.getEditorRef().editor.getBoundingClientRect();
         }
     };
 
